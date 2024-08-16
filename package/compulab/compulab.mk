aa2df084f4fce858088f7072b2373b65fb794bed
abelino <abelino.romo@gmail.com>
2024-08-15 21:57:04 -0700

    Ensure env is setup for imx8-bootloader-prepare.sh

--- package/compulab/compulab.mk
+++ package/compulab/compulab.mk
@@ -8,9 +8,8 @@ COMPULAB_VERSION = 14f548ef600
 COMPULAB_SITE = https://github.com/compulab-yokneam/buildroot/archive
 COMPULAB_SOURCE = $(COMPULAB_VERSION).tar.gz
 
-COMPULAB_DEPENDENCIES = \
-	host-imx-mkimage \
-	host-uboot-tools
+HOST_COMPULAB_DEPENDENCIES = \
+	host-imx-mkimage
 
 HOST_UBOOT_TOOLS_PRE_BUILD_HOOKS += HOST_COMPULAB_PRE_BUILD_HOOK
 HOST_UBOOT_TOOLS_POST_INSTALL_HOOKS += HOST_COMPULAB_POST_INSTALL_HOOK
@@ -20,7 +19,7 @@ define HOST_COMPULAB_PRE_BUILD_HOOK
 endef
 
 define HOST_COMPULAB_POST_INSTALL_HOOK
-	$(HOST_COMPULAB_DIR)/scripts/imx8-bootloader-prepare.sh \
+	$(EXTRA_ENV) $(HOST_COMPULAB_DIR)/scripts/imx8-bootloader-prepare.sh \
 		"unused" $(UBOOT_DIR)/arch/arm/dts/iot-gate-imx8plus.dtb
 endef
 
